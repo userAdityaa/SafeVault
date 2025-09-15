7bbc671b2111ee4fb3555ed0c6ad5560d216f24a
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-09-16 01:12:54 +0530

    user_repo: Repo for user auth activities

---
+++ backend/internal/repository/user_repository.go
@@ -0,0 +1,86 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/useradityaa/internal/models"
+)
+
+type UserRepository interface {
+	FindByEmail(ctx context.Context, email string) (*models.User, error)
+	FindByGoogleMail(ctx context.Context, email string) (*models.GoogleUser, error)
+	Create(ctx context.Context, user *models.User) error
+	CreateGoogleUser(ctx context.Context, user *models.GoogleUser) error
+	FindByID(ctx context.Context, id string) (*models.User, error)
+}
+
+type userRepository struct {
+	DB *pgxpool.Pool
+}
+
+func NewUserRepository(db *pgxpool.Pool) UserRepository {
+	return &userRepository{DB: db}
+}
+
+func (r *userRepository) FindByEmail(ctx context.Context, email string) (*models.User, error) {
+	query :=
+		`SELECT id, email, password_hash
+	FROM users 
+	WHERE email=$1`
+	row := r.DB.QueryRow(ctx, query, email)
+
+	user := &models.User{}
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *userRepository) FindByGoogleMail(ctx context.Context, email string) (*models.GoogleUser, error) {
+	query :=
+		`SELECT id, email
+	FROM google_users 
+	WHERE email=$1`
+	row := r.DB.QueryRow(ctx, query, email)
+
+	user := &models.GoogleUser{}
+	err := row.Scan(&user.ID, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *userRepository) Create(ctx context.Context, user *models.User) error {
+	query :=
+		`INSERT INTO users (id, email, password_hash) VALUES ($1, $2, $3)`
+	_, err := r.DB.Exec(ctx, query, user.ID, user.Email, user.PasswordHash)
+
+	return err
+}
+
+func (r *userRepository) CreateGoogleUser(ctx context.Context, user *models.GoogleUser) error {
+	query := `INSERT INTO google_users (email) VALUES ($1) RETURNING id`
+	return r.DB.QueryRow(ctx, query, user.Email).Scan(&user.ID)
+}
+
+func (r *userRepository) FindByID(ctx context.Context, id string) (*models.User, error) {
+	query :=
+		`SELECT id, email, password_hash 
+	FROM users 
+	WHERE id=$1`
+	row := r.DB.QueryRow(ctx, query, id)
+
+	user := &models.User{}
+
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
